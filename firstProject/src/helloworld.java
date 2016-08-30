ccb71441631b29dc8381eb7ea2700fb50e670007
bleecodes <writeto.blee@gmail.com>
2016-08-29 21:40:39 -0400

    Update helloworld.java

--- firstProject/src/helloworld.java
+++ firstProject/src/helloworld.java
@@ -5,5 +5,6 @@ public class helloworld {
     public static void main (String []args){
         System.out.println("hello");
         System.out.println("something cool happened");
+          System.out.println("tried the SSH approach");
     }
 }
